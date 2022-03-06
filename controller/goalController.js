e1aa12009922a6821e08ad8c9a232f7e42e18fbc
IMRANDIL <aliimranadil2@gmail.com>
2022-03-06 12:24:48 +0530

    yh

--- controller/goalController.js
+++ controller/goalController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require('express-async-handler');
 
-const Goal = require('../models/goal')
+const Goal = require('../models/goal');
+
+const User = require('../models/user')
 
 // @desc Ge Goals
 
@@ -61,6 +63,20 @@ const updateGoal = asyncHandler(async (req, res, next) => {
     }
 
 
+    const user = await User.findById(req.user.id);
+
+    //check for user...
+    if (!user) {
+        res.status(401);
+        throw new Error('User not')
+    }
+    //make sure the logged in user matches..the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('User not authorized')
+    }
+
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
     res.status(200).json(updatedGoal)
@@ -82,6 +98,21 @@ const deleteGoal = asyncHandler(async (req, res, next) => {
     if (!goal) {
         res.status(400);
         throw new Error('Goal not found')
+    }
+
+
+
+    const user = await User.findById(req.user.id);
+
+    //check for user...
+    if (!user) {
+        res.status(401);
+        throw new Error('User not')
+    }
+    //make sure the logged in user matches..the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('User not authorized')
     }
 
 
